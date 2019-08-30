547b552b20a13696dd7d160abb6213a206a66594
busradogann <busradogan1993@gmail.com>
2019-08-30 17:26:27 +0300

    edited package.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.scss";
 
 import Sidebar from "./sidebar/Sidebar";
 import Main from "./main/Main";
 import MainRight from "./main-right/MainRight";
+
 
 export default function App() {
   return (
